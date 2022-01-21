e9331cd60f6d22cf33d6a83a223a6446eb004404
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2022-01-21 11:14:21 +0300

    Добавьте файлы проекта.

---
+++ числа наоборот/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+/*Дано целое число N, найти число, полученное при
+прочтении числа N справа налево. Например, если было
+введено число 345, то программа должна вывести число 543.*/
+namespace числа_наоборот
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+            Console.WriteLine("введите заданное число ");
+             N = Int32.Parse(Console.ReadLine());
+            string s = N.ToString();  //переводим число в строку
+            char[] arr = s.ToCharArray(); //переводим в массив символов
+            Array.Reverse(arr);  // а теперь все написанно за нас нам только использовать 
+            s = new String(arr); //создаем новую строку и передаем наш массив
+            N = Convert.ToInt32(s); // обратно из строки в число
+            Console.WriteLine(s); // ну и выводим на экран 
+        }
+    }
+}
